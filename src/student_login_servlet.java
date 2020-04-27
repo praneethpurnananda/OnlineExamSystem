92452e795819b39279ffe4115a6af9e4db6e8f98
Purnananda Praneeth <praneeth.mandalemula@gmail.com>
2020-04-28 01:01:57 +0530

    first commit

---
+++ src/student_login_servlet.java
@@ -0,0 +1,66 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/student_login_servlet")
+public class student_login_servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+    public student_login_servlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		try {
+		      Class.forName("oracle.jdbc.driver.OracleDriver");
+		      Connection con= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","manager");
+		      PreparedStatement ps=con.prepareStatement("select * from student where studentId=? and password=?");
+		      String un=request.getParameter("t1");
+		      String pw=request.getParameter("t2");
+		      ps.setString(1,un);
+		      ps.setString(2, pw);
+		      ResultSet rs=ps.executeQuery();
+		      response.setContentType("text/html");
+		      PrintWriter out = response.getWriter();
+
+		      if(rs.next())
+		      {
+		      out.println("<h1>Login success</h1>");
+		      }
+		      else
+		      {
+		        out.println("<h1>Invalid Credentials</h1>");
+		      }
+		    }
+		    catch(Exception e) {
+		      System.out.println(e);
+		    }
+		  }
+	}
+
+
---
+++ src/student_register_servlet.java
@@ -0,0 +1,57 @@
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/student_register_servlet")
+public class student_register_servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public student_register_servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+		try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","manager");
+		PreparedStatement ps = con.prepareStatement("insert into student values(?,?,?,?,?)");
+		String studentName = request.getParameter("r1");
+		String studentId = request.getParameter("r2");
+		String mobileNumber = request.getParameter("r3");
+		String institutionId = request.getParameter("r4");
+		String password = request.getParameter("r5");
+		ps.setString(1, studentName);
+		ps.setString(2, studentId);
+		ps.setString(3, mobileNumber);
+		ps.setString(4, institutionId);
+		ps.setString(5, password);
+		ps.executeUpdate();
+		response.setContentType("text/html");
+		
+		response.sendRedirect("studentlogin.html");
+		}
+		
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
